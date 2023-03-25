ab8f06c3a0b7ccbb511980b62a3129306c1cf464
leedom <jld1221@163.com>
2023-03-25 23:37:59 +0800

    refactor: algolia i18n zh-cn

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -354,45 +354,45 @@ const algolia = {
   appId: 'ALTS7M1QY7',
   apiKey: 'dd3f3352651b8c25656774453b128223',
   indexName: 'leedom',
-  // placeholder: '搜索文档',
-  // translations: {
-  //   button: {
-  //     buttonText: '搜索文档',
-  //     buttonAriaLabel: '搜索文档',
-  //   },
-  //   modal: {
-  //     searchBox: {
-  //       resetButtonTitle: '清除查询条件',
-  //       resetButtonAriaLabel: '清除查询条件',
-  //       cancelButtonText: '取消',
-  //       cancelButtonAriaLabel: '取消',
-  //     },
-  //     startScreen: {
-  //       recentSearchesTitle: '搜索历史',
-  //       noRecentSearchesText: '没有搜索历史',
-  //       saveRecentSearchButtonTitle: '保存至搜索历史',
-  //       removeRecentSearchButtonTitle: '从搜索历史中移除',
-  //       favoriteSearchesTitle: '收藏',
-  //       removeFavoriteSearchButtonTitle: '从收藏中移除',
-  //     },
-  //     errorScreen: {
-  //       titleText: '无法获取结果',
-  //       helpText: '你可能需要检查你的网络连接',
-  //     },
-  //     footer: {
-  //       selectText: '选择',
-  //       navigateText: '切换',
-  //       closeText: '关闭',
-  //       // searchByText: '搜索提供者',
-  //     },
-  //     noResultsScreen: {
-  //       noResultsText: '无法找到相关结果',
-  //       suggestedQueryText: '你可以尝试查询',
-  //       openIssueText: '你认为该查询应该有结果？',
-  //       openIssueLinkText: '点击反馈',
-  //     },
-  //   },
-  // }
+  placeholder: '搜索文档',
+  translations: {
+    button: {
+      buttonText: '搜索文档',
+      buttonAriaLabel: '搜索文档',
+    },
+    modal: {
+      searchBox: {
+        resetButtonTitle: '清除查询条件',
+        resetButtonAriaLabel: '清除查询条件',
+        cancelButtonText: '取消',
+        cancelButtonAriaLabel: '取消',
+      },
+      startScreen: {
+        recentSearchesTitle: '搜索历史',
+        noRecentSearchesText: '没有搜索历史',
+        saveRecentSearchButtonTitle: '保存至搜索历史',
+        removeRecentSearchButtonTitle: '从搜索历史中移除',
+        favoriteSearchesTitle: '收藏',
+        removeFavoriteSearchButtonTitle: '从收藏中移除',
+      },
+      errorScreen: {
+        titleText: '无法获取结果',
+        helpText: '你可能需要检查你的网络连接',
+      },
+      footer: {
+        selectText: '选择',
+        navigateText: '切换',
+        closeText: '关闭',
+        searchByText: '搜索供应商',
+      },
+      noResultsScreen: {
+        noResultsText: '无法找到相关结果',
+        suggestedQueryText: '你可以尝试查询',
+        openIssueText: '你认为该查询应该有结果？',
+        openIssueLinkText: '点击反馈',
+      },
+    },
+  }
 }
 
 // 中文
